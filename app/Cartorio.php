c608a0d726542b5172a9bc12a8537513e641adb8
thamirests <thamirestts@hotmail.com>
2017-01-09 14:51:47 -0200

    Add files via upload

--- app/Cartorio.php
+++ app/Cartorio.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Cartorio extends Model
 {
     protected $table = 'tb_cartorio';
+    public $timestamps = false;
 
-    protected $fillable  = ('nome', 'tabeliao', 'telefone', 'email');
+    protected $fillable  = array('nome', 'tabeliao', 'telefone', 'email', 'observacao');
 
     public function propriedade(){
     	return $this->hasMany('fundiario\Propriedade');
--- app/Propriedade.php
+++ app/Propriedade.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Propriedade extends Model
 {
-    protected $table = 'td_propriedades';
+    protected $table = 'tb_propriedades';
 
 	protected $fillable = array('denominacao', 'cep', 'logradouro', 'numero', 'quadra', 'bairro', 'distrito', 'municipio', 'estado', 'pais', 'proprietario', 'area', 'idt_registrado', 'numero_itr', 'numero_ccir', 'dt_geracao','cod_imovel', 'modulos_fiscais', 'num_autencidade', 'registro_car', 'dt_cadastro', 'area_rl', 'cod_cartorio', 'matricula', 'livro', 'folha');
 
