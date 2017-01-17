695fb548c335d7503aad5a40682672d88d55dc27
thamirests <thamirestts@hotmail.com>
2017-01-17 12:34:33 -0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,19 @@ Route::post('/login', 'LoginController@login');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+//cadastro de empreendimento
 Route::get('/cadastro/empreendimento', 'FundiarioController@empreendimento');
+Route::post('/cadastro/cadastraEmpreendimento', 'FundiarioController@cadastraEmpreendimento');
+//cadastro de propriedades
 Route::get('/cadastro/propriedade', 'FundiarioController@propriedade');
 Route::post('/cadastro/cadastraPropriedade', 'FundiarioController@cadastraPropriedade');
+//cadastro de proprietarios
 Route::get('/cadastro/proprietario', 'FundiarioController@proprietario');
+Route::post('/cadastro/cadastraProprietario', 'FundiarioController@cadastraProprietario');
+//teste conexão com o banco de dados
 Route::get('/teste', 'FundiarioController@teste');
+//cadastro de cartório
 Route::get('/cadastro/cartorio', 'FundiarioController@cartorio');
-Route::post('/cadatro/cadastraCartorio', 'FundiarioController@cadastraCartorio');
+Route::post('/cadastro/cadastraCartorio', 'FundiarioController@cadastraCartorio');
+//relacionar proprietário com propriedade
+Route::get('/cadastro/propriedadeProprietario', 'FundiarioController@propriedadeProprietario');
