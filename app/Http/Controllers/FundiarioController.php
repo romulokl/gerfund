0b4500a5c8929710447561c41564ac5c102e630e
thamirests <thamirestts@hotmail.com>
2017-01-17 12:34:55 -0200

    Add files via upload

--- app/Http/Controllers/FundiarioController.php
+++ app/Http/Controllers/FundiarioController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use App\Cartorio;
-use fundiario\Propriedade;
-use Illuminate\Http\Request;
+use App\Propriedade;
+use App\Proprietario;
+use App\Empreendimento;
+//use Illuminate\Http\Request;
+use Request;
 
 
 class FundiarioController extends Controller
@@ -13,16 +16,25 @@ class FundiarioController extends Controller
     public function empreendimento(){
     	return view('cadastro.empreendimento');
     }
+    public function cadastraEmpreendimento(){
+        Empreendimento::create(Request::all());
+        return redirect()->action('FundiarioController@empreendimento')->withInput(Request::only('denominacao'));
+    }
      public function propriedade(){
-    	return view('cadastro.propriedades');//->with('cartorio', Cartorio::all());
+    	return view('cadastro.propriedades')->with('cartorio', Cartorio::all())->with('proprietario', Proprietario::all())->with('empreendimento', Empreendimento::all());
     }
     public function cadastraPropriedade(){
 
         Propriedade::create(Request::all());
-        return redirect()->action('FundiarioController@proprietario')->withInput(Request::only('nome'));
+        return redirect()->action('FundiarioController@propriedade')->withInput(Request::only('denominacao'));
     }   
      public function proprietario(){
     	return view('cadastro.proprietarios');
+    }
+    public function cadastraProprietario(){
+
+        Proprietario::create(Request::all());
+        return redirect()->action('FundiarioController@proprietario')->withInput(Request::only('nome'));
     }
     public function teste(){
     	if(DB::connection()->getDatabaseName())
@@ -36,7 +48,11 @@ class FundiarioController extends Controller
     public function cadastraCartorio(){
 
         Cartorio::create(Request::all());
-        return redirect()->action('cadastro.propriedades');
+        return redirect()->action('FundiarioController@cartorio')->withInput(Request::only('nome'));
     }
-    
+    public function propriedadeProprietario(){
+        
+       
+        return view('cadastro.propriedadeProprietario')->with('propriedades', Propriedade::all())->with('proprietarios', Proprietario::all());
+    }
 }
