0bb15d23ffc77402230a23bdcdbd3100d82b135c
thamirests <thamirestts@hotmail.com>
2017-01-19 15:33:02 -0200

    Add files via upload

--- resources/views/layouts/oficial.blade.php
+++ resources/views/layouts/oficial.blade.php
@@ -1,18 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title></title>	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="/css/app.css">
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.13/css/jquery.dataTables.css">  
+  	<link rel="stylesheet" href="/resources/demos/style.css">
+
 	<script src="jquery.js" type="text/javascript"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script src="jquery.maskedinput.js" type="text/javascript"></script>
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  	<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.13/js/jquery.dataTables.js"></script>
+  	
+
 	
 	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>-->
 </head>
 <body>
-	<div class="container">
+	<div >
+
 		<div class="corpo">
 		<nav class="navbar navbar-inverse barra">
 		<div class="container-fluid">
@@ -51,9 +62,15 @@
 	            
 	          @endif
 		</nav>
-		@yield('conteudo')
+		
 		<div class="container">
 			
+		
+		@yield('conteudo')
+
+		<div class="container">
+			
+		</div>
 		</div>
 		@yield('footer')
 		<footer class="footer barra">
