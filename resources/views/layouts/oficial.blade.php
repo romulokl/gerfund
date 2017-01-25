7703824af1759256fa3c30accd2dcc1dcce9e3d3
thamirests <thamirestts@hotmail.com>
2017-01-25 15:03:14 -0200

    Add files via upload

--- resources/views/layouts/oficial.blade.php
+++ resources/views/layouts/oficial.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>	
+	<title></title>		
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="/css/app.css">
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
@@ -22,7 +22,7 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>-->
 </head>
 <body>
-	<div >
+	<div>
 
 		<div class="corpo">
 		<nav class="navbar navbar-inverse barra">
@@ -33,37 +33,59 @@
 				</div>
 			@if(auth()->guest())
 				<div class="navbar-center">
-					<a href="/" class="navbar-brand">&nbsp Gerenciamento Fundiário </a>
+					<a href="/" class="navbar-brand" style="color: white;">&nbsp Gerenciamento Fundiário </a>
 				</div>
 			</div>
 			@else
-			<div class="navbar-right">
-				<ul>
-					<li><a href=""></a>1</li>
-					<li><a href=""></a>2</li>
-				</ul>
-			</div>
-			<li class="dropdown">
-	              <a  class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ auth()->user()->name }} <span class="caret"></span>
+			<div class="navbar-center">					
+					<a href="{{action('ConsultaController@consultaPropriedade')}}" class="navbar-brand" style="color: white;">&nbsp Gerenciamento Fundiário </a>
+				</div>
+			<ul class="nav navbar-nav navbar-left">
+				
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#" style="color: white;">Cadastro<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="{{action('FundiarioController@empreendimento')}}">Empreendimento</a></li>
+							<li><a href="{{action('FundiarioController@propriedade')}}">Propriedade</a></li>
+							<li><a href="{{action('FundiarioController@proprietario')}}">Proprietário</a></li>
+							<li><a href="{{action('FundiarioController@cartorio')}}">Cartório</a></li>
+						</ul>
+					</li>
+				
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#" style="color: white;">Consulta<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="{{action('ConsultaController@consultaEmpreendimento')}}">Empreendimento</a></li>
+						<li><a href="{{action('ConsultaController@consultaPropriedade')}}">Propriedade</a></li>
+						<li><a href="{{action('ConsultaController@consultaProprietario')}}">Proprietário</a></li>
+					</ul>
+				</li>
+				
+			</ul>
+
+			<div class="dropdown navbar-right" style="padding-top: 10px; padding-right: 10px;">
+	              <a  class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style="color: white;">{{ auth()->user()->name }} <span class="caret"></span>
 	              </a>
 	              <ul class="dropdown-menu" role="menu">
 	                                    <li>
 	                                        <a href="{{ url('/login') }}"
 	                                            onclick="event.preventDefault();
-	                                                     document.getElementById('logout-form').submit();">
+	                                                     document.getElementById('logout-form').submit(); ">
 	                                            Logout
+	                                           
 	                                        </a>
 
 	                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-	                                            {{ csrf_field() }}
+	                                             {!! csrf_field() !!}
 	                                        </form>
 	                                    </li>
-	                                </ul>    
+	                </ul>  
+	        </div>  
 	            
 	          @endif
 		</nav>
 		
-		<div class="container">
+		<div class="container ">
 			
 		
 		@yield('conteudo')
@@ -74,7 +96,7 @@
 		</div>
 		@yield('footer')
 		<footer class="footer barra">
-	      <a href="http:\\www.controlambiental.com.br" target="_BLANK">© Control Ambiental - Sustentabilidade e Meio Ambiente</a>
+	      <a href="http:\\www.controlambiental.com.br" target="_BLANK" style="color: white;">© Control Ambiental - Sustentabilidade e Meio Ambiente</a>
 	 	</footer>
 	 	</div>
 	</div>
