0dd1fd50a471eb1b1537c0c53327cc8d8981e3bc
Romulo de Almeida Kautezky Larder <romulokl@gmail.com>
2017-01-31 15:51:25 -0200

    Add files via upload

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,8 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \App\Http\Middleware\DateNull::class,
+       //\App\Http\Middleware\Autorizador::class,
     ];
 
     /**
@@ -29,7 +31,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,            
         ],
 
         'api' => [
@@ -52,5 +54,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'Autorizador' => \App\Http\Middleware\Autorizador::class,
     ];
 }
